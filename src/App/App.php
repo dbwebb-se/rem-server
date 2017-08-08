89081ed040626dd027af3b26dda460a5f47337c1
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-08 09:28:33 +0200

    testing for ramverk1 kmom02

--- src/App/App.php
+++ src/App/App.php
@@ -4,10 +4,12 @@ namespace Mos\App;
 
 /**
  * An App class to wrap the resources of the framework.
+ *
+ * @SuppressWarnings(PHPMD.ExitExpression)
  */
 class App
 {
-    public function redirect ($url)
+    public function redirect($url)
     {
         $this->response->redirect($this->url->create($url));
         exit;
@@ -25,7 +27,8 @@ class App
         // Add layout, render it, add to response and send.
         $this->view->add("default1/layout", $data, "layout");
         $body = $this->view->renderBuffered("layout");
-        $this->response->setBody($body)->send($status);
+        $this->response->setBody($body)
+                       ->send($status);
         exit;
     }
 }
