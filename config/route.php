4d044bc977145bb6142ef094026f88231cf5f2b7
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-08 12:29:51 +0200

    use older version of the routes

--- config/route.php
+++ config/route.php
@@ -4,5 +4,5 @@
  */
 require __DIR__ . "/route/internal.php";
 require __DIR__ . "/route/base.php";
-//require __DIR__ . "/route/remserver.php";
-$app->router->load(__DIR__ . "/route/remserver.php");
+require __DIR__ . "/route/remserver.php";
+//$app->router->load(__DIR__ . "/route/remserver.php");
